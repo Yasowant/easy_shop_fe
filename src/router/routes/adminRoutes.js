99a5191c3d97a9a65a52a7534284c74cd3b2fd6f
Yasowant <yasowant1998@gmail.com>
2025-01-03 09:54:16 +0530

    live chat and seller dashboard and Seller route setup

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -8,6 +8,8 @@ const PaymentRequest = lazy(() => import('../../views/admin/PaymentRequest'));
 const DeactiveSeller = lazy(() => import('../../views/admin/DeactiveSeller'));
 const SellerRequest = lazy(() => import('../../views/admin/SellerRequest'));
 const SellerDetails = lazy(() => import('../../views/admin/SellerDetails'));
+const ChatSeller = lazy(() => import('../../views/admin/ChatSeller'));
+const OrderDetails = lazy(() => import('../../views/admin/OrderDetails'));
 export const adminRoutes = [
   {
     path: 'admin/dashboard',
@@ -47,6 +49,16 @@ export const adminRoutes = [
   {
     path: 'admin/dashboard/sellers/details/:sellerId',
     element: <SellerDetails />,
+    role: 'admin',
+  },
+  {
+    path: 'admin/dashboard/chat-sellers',
+    element: <ChatSeller />,
+    role: 'admin',
+  },
+  {
+    path: 'admin/dashboard/order/details/:orderId',
+    element: <OrderDetails />,
     role: 'admin',
   },
 ];
