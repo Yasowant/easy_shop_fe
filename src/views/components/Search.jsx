5884a698b6bb4debad72c2f77b3acc941a21ef50
Yasowant <yasowant1998@gmail.com>
2025-01-04 11:48:08 +0530

    search error solved

--- src/views/components/Search.jsx
+++ src/views/components/Search.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Search = ({ setParpage, setSearchValue, searchValue }) => {
+const Search = ({ setParPage, setSearchValue, searchValue }) => {
   return (
     <div className="flex justify-between items-center">
       <select
-        onChange={(e) => setParpage(parseInt(e.target.value))}
+        onChange={(e) => setParPage(parseInt(e.target.value))}
         className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
       >
         <option value="5">5</option>
