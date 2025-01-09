eb68f29f3dfdb3e7e1118232babb2abc44ebd7ad
Yasowant <yasowant1998@gmail.com>
2025-01-09 09:33:30 +0530

    api chages done

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'http://easy-shop-be.onrender.com/api',
+  baseURL: 'https://easy-shop-be.onrender.com/api',
 });
 
 export default api;
