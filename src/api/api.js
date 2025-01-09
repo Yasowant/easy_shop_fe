ecae5c986ab2679d27895995ae94cf85e8e10fa8
Yasowant Nayak <129403851+Yasowant@users.noreply.github.com>
2025-01-09 11:07:21 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://easy-shop-be.onrender.com/api',
-  withCredentials: true, // Ensures cookies are sent
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default api;
